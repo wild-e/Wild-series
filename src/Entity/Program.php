c120222c61ddb926feaee8730d2161e011e84dee
wild-e <wild.emmanuelle@gmail.com>
2020-12-07 16:41:58 +0100

    I added validation to form Program

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -6,10 +6,20 @@ use App\Repository\ProgramRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+// Validation
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=ProgramRepository::class)
+ * @Assert\EnableAutoMapping()
+ * @UniqueEntity(
+ *     fields={"title"},
+ *     errorPath="title",
+ *     message="ce titre existe déjà"
+ * )
  */
+
 class Program
 {
     /**
@@ -20,38 +30,53 @@ class Program
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, unique=true)
+     * @Assert\NotBlank()
+     * @Assert\Length(max="255", min="3")
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank()
+     * @Assert\Regex(
+     *     pattern="/^plus belle la vie$/",
+     *     match=false,
+     *     message="On parle de vraies séries ici")
      */
     private $summary;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
+     * @Assert\NotBlank()
+     * @Assert\Url
+     * @Assert\Length(max="255", min="3")
      */
     private $poster;
 
     /**
      * @ORM\ManyToOne(targetEntity=Category::class)
      * @ORM\JoinColumn(nullable=false)
+     * @Assert\NotBlank()
      */
     private $category;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
+     * @Assert\NotBlank()
+     * @Assert\Length(max="255", min="3")
      */
     private $country;
 
     /**
      * @ORM\Column(type="integer", nullable=true)
+     * @Assert\NotBlank()
      */
     private $year;
 
     /**
      * @ORM\OneToMany(targetEntity=Season::class, mappedBy="program")
+     * @Assert\NotBlank()
      */
     private $season;
 
