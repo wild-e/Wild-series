235c82e27834e03e2b990758ff36019896ab3b3f
wild-e <wild.emmanuelle@gmail.com>
2020-12-08 16:53:20 +0100

    added new actor

--- README.md
+++ README.md
@@ -12,3 +12,5 @@ Video for quest 11 = Quest11.mp4
 Video for quest 12 = Quest12.mp4
 
 Video for quest 13 = Quest13.mp4
+
+Video for quest 14 = Quest134.mp4
--- src/Form/ActorType.php
+++ src/Form/ActorType.php
@@ -6,14 +6,16 @@ use App\Entity\Actor;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 
 class ActorType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('programs')
+            ->add('name', TextType::class)
+            ->add('programs', null, ['choice_label' => 'title']);
         ;
     }
 
